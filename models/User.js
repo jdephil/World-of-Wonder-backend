e174967df405d8a1a5e92aaa991ab9d236518e32
d4vves <d4vves@gmail.com>
2020-07-24 21:24:02 -0500

    added user model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    artifacts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Artifact'
+    }],
+    journalEntries: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Journal'
+    }]
+}, {
+    timestamps: true
+})
+
+module.exports = User = mongoose.model('user', UserSchema)
